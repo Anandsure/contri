7eb9661c81f90b8974901c3359393e99624dd311
unknown-guy-1610 <wwwanandsuresh@gmail.com>
2020-03-07 23:02:41 +0530

    "test push"

--- contribute.py
+++ contribute.py
@@ -1,3 +1,14 @@
 import os
-
-os.system('cmd /c "git add ."')
+import time as t
+import random as rand
+import subprocess
+while True:
+    t.sleep(8)
+    print('starting')
+    f= open("topush.txt","w+")
+    ch = rand.randint(0,9)
+    f.write(str(ch))
+    subprocess.call(["git", "add" , "."])
+    subprocess.call(["git","commit","-m",'''"test push"'''])
+    subprocess.call(["git" , "push"])
+    print('pushed')
